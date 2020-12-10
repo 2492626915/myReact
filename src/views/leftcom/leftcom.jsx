4dc004df3e4ed80e16bfef0b00efbc13e980569d
yangleicheng <2492626915@qq.com>
2020-12-10 17:55:48 +0800

    '初始化项目'

--- src/views/leftcom/leftcom.jsx
+++ src/views/leftcom/leftcom.jsx
@@ -3,17 +3,32 @@ import React, {Component} from 'react';
 export default class LeftCom extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
+    this.state = {
 
+    };
+  }
+  btnClick = () => {
+  }
+    inputChange = (e) => {
+        console.log(e.target.value);
+            this.setState({
+            userName:e.target.value
+            },
+            () => {
+                console.log(this.state.userName);
+            }
+        )
+    }
   render() {
     return (
         <div style={{ float:'left' }}>
-          <div>用户名</div>
-          <input type="text" placeholder="用户名"/>
+          <div >用户名</div>
+          <input ref='userName' onBlur={ (e) => { this.inputChange(e) } } type="text" placeholder="用户名"/>
           <div>评论内容</div>
           <textarea name="" id="" cols="30" rows="10"></textarea>
-          <button>发布</button>
+            <div>
+                <button onClick={ this.btnClick } style={{ width:'150px',height:'30px',background:'#8787ff',border:'none',borderRadius:'20px' }}>发布</button>
+            </div>
         </div>
     )
   }
